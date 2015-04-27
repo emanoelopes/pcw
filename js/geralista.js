1acac12158cadad6afe7c216614fa8bc01bc08a9
Emanoel <emanoelopes@gmail.com>
2015-04-27 07:53:06 -0700

    Adicionando Bootstrap

--- js/geralista.js
+++ js/geralista.js
@@ -2,7 +2,8 @@
 function geralista(){
         var b = parseInt(document.getElementById("a").value);
 		var c = b + 1;//9
-		document.write("<ul>");
+		document.write("<h1>Resultado</h1>");
+        document.write("<ul>");
 		for(i = 0; i < 10 ; i++ ){
                 document.write("<li>" + c +"</li>");
 				c = c + 2;
